4243ef7d18c1f6a8beaf988f93c983b5f6d7ddaf
Kallatti <manjunath.kallatti@hpe.com>
2023-05-18 01:35:56 +0530

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import NavBar from './pages/NavBar/NavBar';
 import PageRoutes from './routes/PageRoutes';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
       <PageRoutes />
     </div>
   );
--- frontend/src/pages/FileStorage/FileList.js
+++ frontend/src/pages/FileStorage/FileList.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ListGroup, Modal, Row, Col } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import folderImage from "./folder.png";
 
 const FileList = () => {
     const [files, setFiles] = useState([]);
     const [selectedFile, setSelectedFile] = useState("");
     const [fileContent, setFileContent] = useState("");
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         const getFileList = async () => {
@@ -21,36 +26,53 @@ const FileList = () => {
 
     const handleFileClick = async (fileId) => {
         try {
-            const response = await axios.get(
-                `http://localhost:8000/api/v1/file/${fileId}`
-            );
+            const response = await axios.get(`http://localhost:8000/api/v1/file/${fileId}`);
             setSelectedFile(fileId);
             setFileContent(response.data.content);
+            setShowModal(true);
         } catch (error) {
             console.error(error);
         }
     };
 
+    const handleCloseModal = () => {
+        setSelectedFile("");
+        setFileContent("");
+        setShowModal(false);
+    };
+
     return (
         <div>
             <h1>File List:</h1>
-            <ul>
-                {files.map((file) => (
-                    <li
-                        key={file.file_id}
-                        onClick={() => handleFileClick(file.file_id)}
-                        style={{ cursor: "pointer", fontWeight: selectedFile === file.file_id ? "bold" : "normal" }}
-                    >
-                        {file.filename}
-                    </li>
+
+            <Row style={{ margin: '30px' }}>
+                {files.map((file, index) => (
+                    <Col key={file.file_id} md={6} lg={4}>
+                        <Card style={{ width: '10rem', marginBottom: '20px' }}>
+                            <Card.Img variant="top" src={folderImage} alt="" />
+                            <ListGroup.Item
+                                onClick={() => handleFileClick(file.file_id)}
+                                action
+                                active={selectedFile === file.file_id}
+                            >
+                                <i className="bi bi-file-text"></i> {file.filename}
+                            </ListGroup.Item>
+                        </Card>
+                    </Col>
                 ))}
-            </ul>
-            {selectedFile && (
-                <div>
-                    <h1>File Content:</h1>
-                    <p>{fileContent}</p>
-                </div>
-            )}
+            </Row>
+
+            <Modal show={showModal} onHide={handleCloseModal} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>File Content</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{fileContent}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
