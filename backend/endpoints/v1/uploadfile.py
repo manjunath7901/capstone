ebc7b582e13b5f2411fb2da16c5bf54b88c4ff1f
Kallatti <manjunath.kallatti@hpe.com>
2023-05-20 00:30:13 +0530

    update

--- backend/endpoints/v1/uploadfile.py
+++ backend/endpoints/v1/uploadfile.py
@@ -1,78 +1,131 @@
 
-# from config.mongodb_conn import collection2
-# from fastapi import APIRouter,HTTPException,Request, UploadFile,File
-# from bson import ObjectId
-# from fastapi.encoders import jsonable_encoder
+# # from config.mongodb_conn import collection2
+# # from fastapi import APIRouter,HTTPException,Request, UploadFile,File
+# # from bson import ObjectId
+# # from fastapi.encoders import jsonable_encoder
 
-# router = APIRouter()
+# # router = APIRouter()
 
-# @router.post("/upload")
-# async def upload_file(file: UploadFile = File(...)):
-#     content = await file.read()
+# # @router.post("/upload")
+# # async def upload_file(file: UploadFile = File(...)):
+# #     content = await file.read()
 
-#     file_dict = {
-#         "filename": file.filename,
-#         "content": content.decode("utf-8")  # Assuming the file is text-based
-#     }
+# #     file_dict = {
+# #         "filename": file.filename,
+# #         "content": content.decode("utf-8")  # Assuming the file is text-based
+# #     }
 
-#     result = await collection2.insert_one(file_dict)
-#     return {"file_id": str(result.inserted_id)}
+# #     result = await collection2.insert_one(file_dict)
+# #     return {"file_id": str(result.inserted_id)}
+
+# # # @router.get("/files")
+# # # async def get_file_list():
+# # #     files = await collection2.find().to_list(length=None)
+# # #     return {"files": files}
+
+# # # @router.get("/file/{file_id}")
+# # # async def get_file(file_id: str):
+# # #     result = await collection2.find_one({"_id": ObjectId(file_id)})
+# # #     if result is None:
+# # #         raise HTTPException(status_code=404, detail="File not found")
+# # #     return {"content": result["content"]}
 
 # # @router.get("/files")
 # # async def get_file_list():
 # #     files = await collection2.find().to_list(length=None)
-# #     return {"files": files}
+# #     serialized_files = []
+# #     for file in files:
+# #         serialized_file = jsonable_encoder(file)
+# #         serialized_file["_id"] = str(serialized_file["_id"])  # Convert ObjectId to string
+# #         serialized_files.append(serialized_file)
+# #     return {"files": serialized_files}
 
 # # @router.get("/file/{file_id}")
 # # async def get_file(file_id: str):
 # #     result = await collection2.find_one({"_id": ObjectId(file_id)})
 # #     if result is None:
 # #         raise HTTPException(status_code=404, detail="File not found")
-# #     return {"content": result["content"]}
+# #     serialized_result = jsonable_encoder(result)
+# #     serialized_result["_id"] = str(serialized_result["_id"])  # Convert ObjectId to string
+# #     return {"content": serialized_result["content"]}
+
+# from config.mongodb_conn import collection2
+# from fastapi import APIRouter, HTTPException,UploadFile,File
+# from bson import ObjectId
+# from typing import List
+# from fastapi import Form
+# import json
+
+# router = APIRouter()
+
+
+# import json
+
+# @router.post("/upload")
+# async def upload_file(file: UploadFile = File(...), authorized_users: str = Form(...)):
+#     content = await file.read()
+#     authorized_users_array = json.loads(authorized_users)  # Parse the JSON string to a list
+#     file_dict = {
+#         "filename": file.filename,
+#         "content": content.decode("utf-8"),  # Assuming the file is text-based
+#         "authorized_users": authorized_users_array
+#     }
+#     result = await collection2.insert_one(file_dict)
+#     return {"file_id": str(result.inserted_id)}
+
 
 # @router.get("/files")
 # async def get_file_list():
 #     files = await collection2.find().to_list(length=None)
 #     serialized_files = []
 #     for file in files:
-#         serialized_file = jsonable_encoder(file)
-#         serialized_file["_id"] = str(serialized_file["_id"])  # Convert ObjectId to string
+#         serialized_file = {
+#             "filename": file["filename"],
+#             "file_id": str(file["_id"])
+#         }
 #         serialized_files.append(serialized_file)
 #     return {"files": serialized_files}
+
 
 # @router.get("/file/{file_id}")
 # async def get_file(file_id: str):
 #     result = await collection2.find_one({"_id": ObjectId(file_id)})
 #     if result is None:
 #         raise HTTPException(status_code=404, detail="File not found")
-#     serialized_result = jsonable_encoder(result)
-#     serialized_result["_id"] = str(serialized_result["_id"])  # Convert ObjectId to string
-#     return {"content": serialized_result["content"]}
+#     return {"content": result["content"]}
 
 from config.mongodb_conn import collection2
-from fastapi import APIRouter, HTTPException,UploadFile,File
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from bson import ObjectId
 from typing import List
 from fastapi import Form
 import json
+from cryptography.fernet import Fernet
 
 router = APIRouter()
 
+# Generate a key for encryption and decryption
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
-import json
+def encrypt_content(content: str) -> bytes:
+    return cipher_suite.encrypt(content.encode("utf-8"))
+
+def decrypt_content(content: bytes) -> str:
+    return cipher_suite.decrypt(content).decode("utf-8")
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...), authorized_users: str = Form(...)):
     content = await file.read()
+    encrypted_content = encrypt_content(content.decode("utf-8"))
     authorized_users_array = json.loads(authorized_users)  # Parse the JSON string to a list
     file_dict = {
         "filename": file.filename,
-        "content": content.decode("utf-8"),  # Assuming the file is text-based
+        "content": encrypted_content,
         "authorized_users": authorized_users_array
     }
     result = await collection2.insert_one(file_dict)
     return {"file_id": str(result.inserted_id)}
-
 
 @router.get("/files")
 async def get_file_list():
@@ -86,10 +139,10 @@ async def get_file_list():
         serialized_files.append(serialized_file)
     return {"files": serialized_files}
 
-
 @router.get("/file/{file_id}")
 async def get_file(file_id: str):
     result = await collection2.find_one({"_id": ObjectId(file_id)})
     if result is None:
         raise HTTPException(status_code=404, detail="File not found")
-    return {"content": result["content"]}
+    decrypted_content = decrypt_content(result["content"])
+    return {"content": decrypted_content}
